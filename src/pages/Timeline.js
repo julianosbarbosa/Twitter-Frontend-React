79cbbb3e7b09aa4109ad9604956c9d35fd0dfad2
Juliano <julianotrabalho2016@gmail.com>
2019-10-17 19:24:49 -0300

    UPDATE - finished twitterapp

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import './Timeline.css'
-import twitterLogo from '../twitter.svg'
+import './Timeline.css';
+import twitterLogo from '../twitter.svg';
+import api from '../services/api';
+import Tweet from '../components/Tweet';
+import socket from 'socket.io-client';
 
 export default class Timeline extends Component {
 
   state = {
+    tweets: [],
     newTweet: ''
   }
 
-  handleNewTweet = (e)=>{
+  async componentDidMount(){
+    this.subscribeToEvents();
+
+    const response  = await api.get('tweets');
+    this.setState({tweets: response.data})
+  }
+
+  subscribeToEvents = ()=>{
+    const io = socket('http://localhost:3333');
+
+    io.on('tweet', data => {
+      this.setState({tweets: [data, ...this.state.tweets]})
+    })
+    io.on('like', data => {
+      this.setState({tweets: this.state.tweets.map(tweet=>(
+        tweet._id===data._id? data: tweet
+      ))})
+    })
+  }
+
+  handleNewTweet = async (e) =>{
     if(e.keyCode !==13){
       return;
     }
     const content = this.state.newTweet;
     const author = localStorage.getItem('@GoTwitter:username')
 
-    console.log(content, author)
+    await api.post('tweets', {content, author})
+    this.setState({ newTweet:'' })
   }
 
   handleChange = (e)=>{
@@ -24,7 +49,7 @@ export default class Timeline extends Component {
 
   render() {
 
-    const {newTweet} = this.state
+    const {newTweet, tweets} = this.state
 
     return (
       <div className="timeline-wrapper">
@@ -37,6 +62,11 @@ export default class Timeline extends Component {
               onChange={this.handleChange} 
               />
         </form>
+        <ul className="tweet-list">
+          {tweets.map((tweet)=>(
+            <Tweet key={tweet._id} tweet={tweet}/>
+          ))}
+        </ul>
       </div>
     )
   }
