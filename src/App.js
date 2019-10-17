e7f03fd3fcd7855f2ef68f029e587feb4e7e34e0
Juliano <julianotrabalho2016@gmail.com>
2019-10-17 16:39:31 -0300

    UPDATE - configurate react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Timeline from './pages/Timeline';
 
 class App extends Component {
   render(){
-    return <h1>Hello World</h1>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login}/>
+          <Route path="/timeline" component={Timeline}/>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
 
