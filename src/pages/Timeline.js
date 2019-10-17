e2d77f69f05f384033689cc03a9d613b48a8c126
Juliano <julianotrabalho2016@gmail.com>
2019-10-17 17:41:11 -0300

    UPDATE - finished Timeline

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
-
-// import { Container } from './styles';
+import './Timeline.css'
+import twitterLogo from '../twitter.svg'
 
 export default class Timeline extends Component {
+
+  state = {
+    newTweet: ''
+  }
+
+  handleNewTweet = (e)=>{
+    if(e.keyCode !==13){
+      return;
+    }
+    const content = this.state.newTweet;
+    const author = localStorage.getItem('@GoTwitter:username')
+
+    console.log(content, author)
+  }
+
+  handleChange = (e)=>{
+    this.setState({newTweet: e.target.value})
+  }
+
   render() {
-    return <h1>Timeline</h1>
+
+    const {newTweet} = this.state
+
+    return (
+      <div className="timeline-wrapper">
+        <img src={twitterLogo} height={24} alt="Logo"/>
+        <form>
+          <textarea 
+              placeholder="O que está acontecendo?"
+              value={newTweet}
+              onKeyDown={this.handleNewTweet}
+              onChange={this.handleChange} 
+              />
+        </form>
+      </div>
+    )
   }
 }
