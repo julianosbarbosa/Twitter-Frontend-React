1b953facd6e04c3bce4dc47aad2f2c3a8cd8dd57
Juliano <julianotrabalho2016@gmail.com>
2019-10-17 17:12:33 -0300

    UPDATE - twitter login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
-
-// import { Container } from './styles';
+import twitterLogo from '../twitter.svg'
+import './Login.css';
 
 export default class Login extends Component {
+
+  state = {
+    username: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({username: e.target.value})
+  }
+
+  handleOnSubmit = (e)=>{
+    e.preventDefault();
+    const {username} = this.state
+    if(!username.length){
+      return;
+    }
+    localStorage.setItem('@GoTwitter:username', username);
+    this.props.history.push('/timeline')
+  }
+
   render() {
-    return <h1>Login</h1>
+    const {username} = this.state
+    return (
+      <div className="login-wrapper">
+        <img src={twitterLogo} alt="Logo"/>
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text" 
+                 placeholder="Nome do usuario" 
+                 onChange={this.handleChange}
+                 value={username}/>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    )
   }
 }
